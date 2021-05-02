d3a3007ae7d6f6800de5e988f78d4647c52cee1c
Manoj V <46310861+manojv2806@users.noreply.github.com>
2021-05-02 11:37:12 +0530

    Delete Node Setup directory

--- Node Setup/controller/controller1.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    test1 : function(req, res){
-        console.log("req.body");
-        console.log(req);
-        res.send({
-            "status" : "suceess"
-        })
-    }
-}
--- Node Setup/routes/route2.js
+++
@@ -1,6 +0,0 @@
-var express = require('express');
-var app = express();
-const routes = require('express').Router();
-var controller1 = require('../controller/controller1.js');
-app.get('/test1', controller1.test1);
-module.exports = app;
